f685c85f1d1533839e8d4d078b4ac598ffe750bc
Brendan Kelly <brendank@upenn.edu>
2020-04-28 21:44:38 -0400

    Format HTML table rownames.

--- scripts/m_table1.R
+++ scripts/m_table1.R
@@ -50,19 +50,62 @@ subject_summary %>%
          # mero_before0_7,
          contains('_before0_7'),
          ) %>%
+  mutate(ss1_7 = ifelse(ss1_7, "More than 7 Suction Episodes", "7 or Fewer Suction Episodes"),
+         gender = ifelse(gender == "M", "Male", "Female"),
+         race = stringr::str_to_title(string = race)) %>%
+  rename(`Age (years)` = age,
+         Gender = gender,
+         Race = race,
+         # comorbidities
+         `Chronic Obstructive Pulmonary Disease (COPD)` = copd,
+         Asthma = asthma,
+         `Interstitial Lung Disease (ILD)` = ild,
+         `Lymphoma or Leukemia` = lymphleuk,
+         `Diabetes mellitus` = dm,
+         `Congestive Heart Failure (CHF)` = chf,
+         Cirrhosis = cirrhosis,
+         # features of critical illness
+         `Subglottic Suction Events at MV Onset` = ss1,
+         `Duration of MV (days)` = vent_duration,
+         `Ventilator Day of VAE` = vae_date_rel,
+         IVAC = ivac_yn,
+         PVAP = pvap_yn,
+         `Fever Week Before MV Onset` = fever_7before_vent,
+         `Respiratory Culture Order Week Before MV Onset` = respcx_7before_vent,
+         `WBC at MV Onset (1e3 cells/mm3)` = admit_wbc,
+         `Serum Cr at MV Onset (mg/dL)` = admit_cr,
+         `AST at MV Onset (units/L)` = admit_ast,
+         `ALT at MV Onset (units/L)` = admit_alt,
+         `Maximum FiO2 at MV Onset (%)` = admit_fio2,
+         `Maximum PEEP at MV Onset (cm H2O)` = admit_peep,
+         `Norepinephrine at MV Onset` = norepi_after0_1,
+         `Epinephrine at MV Onset` = epi_after0_1,
+         `Vasopressin at MV Onset` = pit_after0_1,
+         `Dopamine at MV Onset` = dopa_after0_1,
+         `Vancomycin (IV) in Week Before MV Onset` = vanco_before0_7,
+         `Metronidazole in Week Before MV Onset` = metro_before0_7,
+         `Linezolid in Week Before MV Onset` = linez_before0_7,
+         `Cefazolin in Week Before MV Onset` = cefaz_before0_7,
+         `Daptomycin in Week Before MV Onset` = dapto_before0_7,
+         `Piperacillin-tazobactam in Week Before MV Onset` = piptaz_before0_7,
+         `Cefepime in Week Before MV Onset` = cefep_before0_7,
+         `Meropenem in Week Before MV Onset` = mero_before0_7,
+         ) %>%
   tbl_summary(
     data = .,
     #by = ivac_yn,
     #by = vae_late,
     by = ss1_7,
-    type = list("admit_fio2" ~ "continuous", "admit_peep" ~ "continuous")
+    type = list(`Maximum FiO2 at MV Onset (%)` ~ "continuous",
+                `Maximum PEEP at MV Onset (cm H2O)` ~ "continuous")
   ) %>%
   add_n() %>%
   add_overall() %>%
   add_p(pvalue_fun = function(x) style_pvalue(x, digits = 2)) %>%
   gtsummary::as_gt() %>%
-  gt::tab_header(title = "Table 1: Sputum Production at Onset of Mechanical Ventilation",
-                 subtitle = "Greater Than 7 Subglottic Suction Episodes in First Calendar Day") -> t1_ss1
+  gt::tab_header(title = "Table 1: Characteristics of Enrolled Subjects",
+                 #subtitle = "Exposure: Greater Than 7 Subglottic Suction Episodes in First Calendar Day",
+                 ) -> t1_ss1
 
 t1_ss1
 
